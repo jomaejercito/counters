31aa22491957037c7c568e5533fce2a5272996db
Joma Ejercito <jomaejercito@gmail.com>
2020-02-27 02:46:27 -0500

    Set up increase and decrease buttons in Counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
 class Counter extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      counter: 0
+    }
+  }
 
   render() {
     return (
       <div className="counter">
-        <h1>Hello!</h1>
+        <h1>{this.state.counter}</h1>
+        <button>Increase</button>
+        <button>Decrease</button>
       </div>
     )
   }
